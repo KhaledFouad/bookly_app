0f11400c9afd846332935b73ffdface2c4dc3190
khaledFouad <khaledfouad6897@gmail.com>
2024-09-03 18:12:19 +0300

    first

---
+++ lib/features/Splash_view/presentation/views/splash.dart
@@ -0,0 +1,13 @@
+import 'package:bookly_app/features/Splash_view/presentation/views/widgets/splashViewBody.dart';
+import 'package:flutter/material.dart';
+
+class Splash extends StatelessWidget {
+  const Splash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Splashviewbody(),
+    );
+  }
+}
